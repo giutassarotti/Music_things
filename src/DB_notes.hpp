bdf09ce920e268cd815ade5ada63e78cac1e4281
Giulia Tassarotti <giuliatassa@gmail.com>
2022-02-10 19:49:32 +0100

    sigh again

--- src/DB_notes.hpp
+++ src/DB_notes.hpp
@@ -5,12 +5,23 @@
 
 using std::vector;
 
+/*
+
+    LEGENDA:
+        0 chiave
+        1 armatura
+        2 tempo
+        3 nota
+        4 barra
+        5 pausa
+
+*/
+
 namespace music
 {
     struct DB_notes
     {
-        static inline const vector<int> notablablabla;
-        static inline const vector blablabla;
+        static inline const vector<vector<int> aldo;
     };
 }
 
